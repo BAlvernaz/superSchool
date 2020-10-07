c332bb2a69c54312778ddf08805ce97ff9990dcc
Blake Alvernaz <balverna@gmail.com>
2020-10-07 16:47:43 -0700

    Started Frontend

--- students/urls.py
+++ students/urls.py
@@ -1,10 +1,13 @@
+
 from django.urls import path
 from students import views
+
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('students/', views.StudentList.as_view()),
-    path('students/<str:pk>/', views.StudentDetail.as_view())
+    path('', views.StudentList.as_view()),
+    path('<str:pk>/', views.StudentDetail.as_view()),
+    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
