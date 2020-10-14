163a87f063d2d339a6e874958901e43d7027d2d6
Blake Alvernaz <balverna@gmail.com>
2020-10-14 15:55:35 -0700

    Added a Drawer/Sidebar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import React from 'react'
-import {HashRouter as Router, Route} from 'react-router-dom'
-import NewStudentForm from './components/NewStudentForm'
-import StudentList from './components/StudentList'
-import { connect } from 'react-redux'
-import { getStudents, getSchools } from './reducers/actions'
-
-import Navbar from './components/Navbar'
-import SchoolList from './components/SchoolList'
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import NewStudentForm from "./components/NewStudentForm";
+import StudentList from "./components/StudentList";
+import { connect } from "react-redux";
+import { getStudents, getSchools } from "./reducers/actions";
+import Navbar from "./components/Navbar";
+import SchoolList from "./components/SchoolList";
+import { Drawer, Paper } from "@material-ui/core";
+import { toggleSideMenu } from "./reducers/toggleActions";
 
 class App extends React.Component {
-    componentDidMount() {
-        this.props.loadStudents()
-        this.props.loadSchools()
-    }
+  componentDidMount() {
+    this.props.loadStudents();
+    this.props.loadSchools();
+  }
 
-    render() {
-        return (
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Drawer
+              open={this.props.sideMenu}
+              variant="temporary"
+              anchor="left"
+              onClose={() => {this.props.sideMenuToggle()}}
+            >
+              <Paper>
+                
+              </Paper>
+            </Drawer>
+
             <div>
-                <Router>
-                    <Route component={Navbar} />
-                    <Route component={StudentList} />
-                    <Route component={NewStudentForm} />
-                    <Route component={SchoolList} />
-                </Router>
+              <Route component={Navbar} />
+              <Route component={StudentList} />
+              <Route component={NewStudentForm} />
+              <Route component={SchoolList} />
             </div>
-        )
-    }
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-const dispatchToProp = dispatch => {
-    return {
-        loadStudents: () => dispatch(getStudents()),
-        loadSchools: () => dispatch(getSchools())
-    }
-}
+const stateToProps = ({ toggles }) => {
+  return {
+    sideMenu: toggles.sideMenu,
+  };
+};
 
-export default connect(null, dispatchToProp)(App)
+const dispatchToProp = (dispatch) => {
+  return {
+    loadStudents: () => dispatch(getStudents()),
+    loadSchools: () => dispatch(getSchools()),
+    sideMenuToggle: () => dispatch(toggleSideMenu()),
+  };
+};
+
+export default connect(stateToProps, dispatchToProp)(App);
