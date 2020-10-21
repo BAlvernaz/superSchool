d4f04f7fb46a71b9575136941f5c332a1e2852b7
Blake Alvernaz <balverna@gmail.com>
2020-10-21 11:48:08 -0700

    Edit and Delete Buttons Student Cards

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,26 +1,45 @@
-import { Card, Typography, CardContent } from "@material-ui/core";
+import { Card, Typography, CardContent, IconButton } from "@material-ui/core";
 import React from "react";
 import { connect } from "react-redux";
 import { removeStudent } from "../reducers/actions";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const StudentList = ({ students, deleteStudent, schools }) => {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "row"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
       {students.length > 0 ? (
         students.map((student) => (
-          <Card  style={{
-            minWidth:247
-            }}>
+          <Card
+            style={{
+              minWidth: 247,
+            }}
+          >
             <CardContent>
-            <Typography style={{ fontSize: 14}}>
-              {"Name: " + student.name}
-            </Typography>
-            <Typography>
-             {"Attending: " + schools.find(school => school.id === student.school).name}
-            </Typography>
+              <Typography style={{ fontSize: 26 }}>
+                {"Name: " + student.name}
+              </Typography>
+              <Typography>
+                {"Attending: " +
+                  schools.find((school) => school.id === student.school).name}
+              </Typography>
+              <div
+                style={{
+                  display: "flex",
+                }}
+              >
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+                <IconButton>
+                  <DeleteIcon />
+                </IconButton>
+              </div>
             </CardContent>
           </Card>
         ))
@@ -33,7 +52,7 @@ const StudentList = ({ students, deleteStudent, schools }) => {
 const stateToProps = ({ students, schools }) => {
   return {
     students,
-    schools
+    schools,
   };
 };
 
