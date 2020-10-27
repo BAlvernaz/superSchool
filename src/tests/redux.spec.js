e3fabec4c55306e617df181c6b3797dbaf45d726
Blake Alvernaz <balverna@gmail.com>
2020-10-27 11:44:26 -0700

    Added more Test

--- src/tests/redux.spec.js
+++ src/tests/redux.spec.js
@@ -1,21 +1,81 @@
-import { v4 as uuidv4 } from 'uuid';
-import { ADD_STUDENT, GET_STUDENTS } from '../reducers/actions';
-import { studentReducer } from '../reducers/studentReducer';
+import { v4 as uuidv4 } from "uuid";
+import { ADD_STUDENT, GET_STUDENTS, REMOVE_STUDENT } from "../reducers/actions";
+import { studentReducer } from "../reducers/studentReducer";
 
-
-describe('Students Reducer', () => {
-    test('Returns Initial State', () => {
-        expect(studentReducer(undefined, {type: GET_STUDENTS, students: []})).toEqual([])
-    })
-    test('Add Students', () => {
-        const student1UUID = uuidv4()
-        const school1UUID = uuidv4()
-        expect(studentReducer(undefined, {type: ADD_STUDENT, student: {
+describe("Students Reducer", () => {
+    const student1UUID = uuidv4();
+    const school1UUID = uuidv4();
+    const student2UUID = uuidv4();
+  test("Returns Initial State", () => {
+    expect(
+      studentReducer(undefined, { type: GET_STUDENTS, students: [] })
+    ).toEqual([]);
+  });
+  test("Add Students", () => {
+    expect(
+      studentReducer(undefined, {
+        type: ADD_STUDENT,
+        student: {
+          id: student1UUID,
+          name: "Test Student 1",
+          school: school1UUID,
+        },
+      })
+    ).toEqual([
+      {
+        id: student1UUID,
+        name: "Test Student 1",
+        school: school1UUID,
+      },
+    ]);
+    expect(
+      studentReducer(
+        [
+          {
             id: student1UUID,
             name: "Test Student 1",
-            school: school1UUID}})).toEqual([{
-                id: student1UUID,
-                name: "Test Student 1",
-                school: school1UUID}])
-    })
-})
+            school: school1UUID,
+          },
+        ],
+        {
+          type: ADD_STUDENT,
+          student: {
+            id: student2UUID,
+            name: "Test Student 2",
+            school: school1UUID,
+          },
+        }
+      )
+    ).toEqual([
+      {
+        id: student1UUID,
+        name: "Test Student 1",
+        school: school1UUID,
+      },
+      {
+        id: student2UUID,
+        name: "Test Student 2",
+        school: school1UUID,
+      }
+    ]);
+  });
+  test("Remove Students", () => {
+       expect(studentReducer([
+        {
+          id: student1UUID,
+          name: "Test Student 1",
+          school: school1UUID,
+        },
+        {
+          id: student2UUID,
+          name: "Test Student 2",
+          school: school1UUID,
+        }
+      ], {type: REMOVE_STUDENT, studentId: student2UUID})).toEqual([
+        {
+          id: student1UUID,
+          name: "Test Student 1",
+          school: school1UUID,
+        }])
+  })
+});
