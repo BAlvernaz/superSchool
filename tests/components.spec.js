e984e3e7e8853462e9695a5c80e277482dababe4
Blake Alvernaz <balverna@gmail.com>
2020-11-02 20:01:57 -0800

    Completed First Component Testing

--- tests/components.spec.js
+++ tests/components.spec.js
@@ -1,32 +1,43 @@
-import React from 'react';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
- 
+import React from "react";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import thunk from "redux-thunk";
+
 configure({ adapter: new Adapter() });
 
-import { shallow, mount, render } from 'enzyme';
-import configureStore from 'redux-mock-store'
-
-const mockStore = configureStore();
-
-import Navbar from '../src/components/Navbar';
-import App from '../src/App'
-
+import configureStore from "redux-mock-store";
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+import { createMount, createShallow } from "@material-ui/core/test-utils";
+import Navbar from "../src/components/Navbar";
+import App from "../src/App";
+import { MemoryRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import StudentList from "../src/components/StudentList";
 
 describe("Component Testing", () => {
-    let wrapper;
-    describe("App Component", () => {
-
-        test('Contains a Navbar', () => {
-            const initState = {
-                toggles: {
-                    editStudentDialog: false
-                }
-            }
-            const store = mockStore(initState)
-            wrapper = shallow(<App store={store}/>)
-            const component = wrapper.dive()
-            expect(component.find("Router").length).toEqual(1)
-        })
-    })
-})
+  let wrapper;
+  describe("App Component", () => {
+    test("Displays Navbar at Any Route", () => {
+      const initState = {
+        toggles: {
+          editStudentDialog: false,
+        },
+        schools : []
+      };
+      const store = mockStore(initState);
+      const shallow = createShallow({ untilSelector: "App" });
+      const mount = createMount();
+      const wrapper = mount(
+        <MemoryRouter initialEntries={["/"]}>
+            <Provider store={store}>
+          <App  />
+          </Provider>
+        </MemoryRouter>
+      );
+      expect(
+        wrapper.find(Navbar).length
+      ).toEqual(1);
+    });
+  });
+});
