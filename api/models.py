cf8a6fab171a00af394606cf23332af202971690
Blake Alvernaz <balverna@gmail.com>
2020-11-12 16:20:56 -0800

    Started New User Model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
 
 class School(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
